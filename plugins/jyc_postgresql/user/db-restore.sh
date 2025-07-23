e2d9afeb4255244838b25a0533237db3b252594a
jyc.dev <jycouet@gmail.com>
2025-07-23 20:36:29 +0200

    Update db-restore.sh

--- plugins/jyc_postgresql/user/db-restore.sh
+++ plugins/jyc_postgresql/user/db-restore.sh
@@ -6,6 +6,8 @@ source "$jyc_postgresql_dir/helpers.sh"
 
 ## ---Your custom things---
 
+# DB_NAME="demo_db" # or in your devbox.json "env": { "DB_NAME": "main_db" }
+
 BACKUP_FILE="../backup/db.$DB_NAME.dmp"
 
 execute_and_log "Database restore" "pg_restore -U postgres -d \"$DB_NAME\" \"$BACKUP_FILE\""
