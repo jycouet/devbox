38febe7bcab08aa60d0dbc54f34b2a8a629af2fb
jyc.dev <jycouet@gmail.com>
2025-07-23 20:36:05 +0200

    Update db-drop.sh

--- plugins/jyc_postgresql/user/db-drop.sh
+++ plugins/jyc_postgresql/user/db-drop.sh
@@ -6,6 +6,6 @@ source "$jyc_postgresql_dir/helpers.sh"
 
 ## ---Your custom things---
 
-DB_NAME="demo_db"
+# DB_NAME="demo_db" # or in your devbox.json "env": { "DB_NAME": "main_db" }
 
 execute_and_log "Database drop if exist" "dropdb --if-exists \"$DB_NAME\""
